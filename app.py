1c14bdc71dcb648170bd26ab0a43734cd1b8f2f3
Oleksii Bielichenko <byelichenko@gmail.com>
2023-03-29 23:36:02 +0300

    done

--- app.py
+++ app.py
@@ -3,19 +3,34 @@ from func_tools.sum import sum
 from func_tools.subtruct import subtruct
 from func_tools.multiply import multiply
 
-is_running = True
 
 # 1) Calculator -> + , * , / , -
 # 2) square
 # 3) Error handling : try , except , finally
 
-while is_running:
-    user_choose = input("Do you want to work with calc? y/n : ")
+while True:
 
-    if user_choose == "y":
-        num1 = input()
-        num2 = input()
-    elif user_choose == "n":
-        is_running = False
-    else:
-        continue
+    operand = input("Enter operand (+, -, *, /, ^2): ")
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        if operand == '+':
+            result = sum(num1, num2)
+        elif operand == '-':
+            result = subtruct(num1, num2)
+        elif operand == '*':
+            result = multiply(num1, num2)
+        elif operand == '/':
+            result = divide(num1, num2)
+        else:
+            print("!!!Wrong operand!!!")
+            continue
+        print(f"Result: {num1} {operand} {num2} = ", result)
+        is_quit = input("Exit (y): ")
+        if is_quit.lower() == 'y':
+            exit()
+    except ValueError:
+        print("!!!Wrong number!!!")
+    except NameError:
+        print("!!!Wrong number!!!")
